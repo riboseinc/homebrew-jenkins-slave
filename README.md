ab4301ec5458125960afb310de177314e21526f2
Erick Borsboom <erick.borsboom@ribose.com>
2017-12-11 15:57:30 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# Homebrew for jenkins-slave
+
+This tap contains a formula to install the Jenkins Slave on macOS.
+
+
+## Quick Install
+
+Run this and it's all done.
+
+``` sh
+brew tap riboseinc/jenkins-slave
+brew install --HEAD jenkins-slave
+```
+
+Or:
+
+``` sh
+brew install --HEAD riboseinc/jenkins-slave/jenkins-slave
+```
+
+## Install Tap
+
+``` sh
+brew tap riboseinc/jenkins-slave
+```
+
+## Install jenkins-slave
+
+Currently it is a HEAD-only formula, i.e., you must install it using the `--HEAD` option:
+
+``` sh
+brew install --HEAD jenkins-slave
+```
+
+If the formula conflicts with one from `Homebrew/homebrew` or another
+tap, you can run:
+
+``` sh
+brew install --HEAD riboseinc/jenkins-slave/jenkins-slave
+```
+
+You can also install via URL:
+
+``` sh
+brew install --HEAD https://raw.githubusercontent.com/riboseinc/homebrew-jenkins-slave/master/jenkins-slave.rb
+```
+
+## Acceptable Formulae
+
+You can read Homebrew’s Acceptable Formulae document [here](https://github.com/Homebrew/brew/blob/master/docs/Acceptable-Formulae.md).
+
+## Troubleshooting
+
+First, please run `brew update` and `brew doctor`.
+
+Second, read the [Troubleshooting Checklist](https://github.com/Homebrew/brew/blob/master/docs/Troubleshooting.md#troubleshooting).
+
+## More Documentation
+
+`brew help`, `man brew` or check [our documentation](https://github.com/Homebrew/brew/tree/master/docs#readme).
+
+## License
+
+Code is under the [BSD 2 Clause license](https://github.com/Homebrew/brew/tree/master/LICENSE.txt).
---
+++ jenkins-slave.rb
@@ -0,0 +1,86 @@
+class JenkinsSlave < Formula
+
+  desc "Jenkins Slave for macOS"
+  homepage "https://jenkins.io/projects/remoting/"
+  url "http://repo.jenkins-ci.org/releases/org/jenkins-ci/main/remoting/3.14/remoting-3.14.jar"
+  sha256 "2539c1ace877de6bc4c013d59ff68548024d04b178bd74c24cff0374c28099dc"
+
+  depends_on :java => "1.8+"
+  bottle :unneeded
+
+  def install
+    libexec.install "remoting-#{version}.jar"
+    bin.write_jar_script libexec/"remoting-#{version}.jar", "remoting"
+  end
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+	<key>UserName</key>
+	<string>#{Process.uid}</string>
+        <array>
+          <string>/usr/bin/java</string>
+          <string>-jar</string>
+          <string>#{libexec}/remoting.jar</string>
+          <string>--jnlpUrl=REPLACE_ME_JENKINS_URL</string>
+          <string>--secret=REPLACE_ME_JENKINS_SECRET</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>KeepAlive</key>
+	<true/>
+        <key>StandardErrorPath</key>
+        <string>#{var}/log/jenkins-slave.log</string>
+        <key>StandardOutPath</key>
+        <string>#{var}/log/jenkins-slave.log</string>
+        <key>SessionCreate</key>
+        <true/>
+      </dict>
+    </plist>
+    EOS
+  end
+
+  def plist_name
+    "com.ribose.jenkins.slave"
+  end
+
+  def caveats; <<-EOS.undent
+    WARNING:
+      You must configure the JENKINS_URL and JENKINS_SECRET variables in the plist file:
+
+    Step 1: Configure your JENKINS_URL / JENKINS_SECRET.
+
+    export JENKINS_SLAVE_PLIST=#{prefix/(plist_name+".plist")}
+    sed -i.bak \\
+      's/REPLACE_ME_JENKINS_URL/https:\\/\\/my-jenkins.com\\/computer\\/agentname\\/slave-agent.jnlp// \\
+      ${JENKINS_SLAVE_PLIST}
+
+    sed -i.bak \\
+      's/REPLACE_ME_JENKINS_SECRET/bd38130d1412b54287a00a3750bd100c/' \\
+      ${JENKINS_SLAVE_PLIST}
+
+    Step 2: Start the Jenkins Slave via brew services
+
+      If you want to start on machine boot, use `root`.
+
+      sudo brew services start riboseinc/jenkins-slaves/jenkins-slave
+
+      If you want to start on login, just do this.
+
+      brew services start riboseinc/jenkins-slaves/jenkins-slave
+
+
+    *Ignore what brew tells you below!*
+    -------------vvvvvvv-------------
+     \------------vvvvv------------/
+      \------------vvv------------/
+       \------------v------------/
+    EOS
+  end
+
+end
