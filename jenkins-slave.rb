7e720851dee912cfd6aa6a6c005774edbc5e6283
Sven Strittmatter <Sven.Strittmatter@iteratec.com>
2020-10-26 21:04:50 +0100

    Check w/ brew audit complained this
    Signed-off-by: Sven Strittmatter <Sven.Strittmatter@iteratec.com>

--- jenkins-slave.rb
+++ jenkins-slave.rb
@@ -17,76 +17,78 @@ class JenkinsSlave < Formula
     "org.jenkins-ci.remoting"
   end
 
-  def caveats; <<~EOS
-    WARNING:
-      You must configure the JENKINS_URL and JENKINS_SECRET variables in the plist file:
+  def caveats
+    <<~EOS
+      WARNING:
+        You must configure the JENKINS_URL and JENKINS_SECRET variables in the plist file:
 
-    Step 1: Set your JENKINS_URL / JENKINS_SECRET in the environment:
+      Step 1: Set your JENKINS_URL / JENKINS_SECRET in the environment:
 
-      export url="https://my-jenkins.com/computer/agentname/slave-agent.jnlp"
-      export secret="bd38130d1412b54287a00a3750bd100c"
+        export url="https://my-jenkins.com/computer/agentname/slave-agent.jnlp"
+        export secret="bd38130d1412b54287a00a3750bd100c"
 
-    Step 2: Insert the JENKINS_URL / JENKINS_SECRET in the plist and ensure that JENKINS_SECRET is not stored in the bash history:
+      Step 2: Insert the JENKINS_URL / JENKINS_SECRET in the plist and ensure that JENKINS_SECRET is not stored in the bash history:
 
-      sed -i "" "s@REPLACE_ME_JENKINS_URL@${url}@" #{prefix/(plist_name+".plist")}
-      sed -i "" "s@REPLACE_ME_JENKINS_SECRET@${secret}@" #{prefix/(plist_name+".plist")}
-      unset HISTFILE url secret
+        sed -i "" "s@REPLACE_ME_JENKINS_URL@${url}@" #{prefix/(plist_name+".plist")}
+        sed -i "" "s@REPLACE_ME_JENKINS_SECRET@${secret}@" #{prefix/(plist_name+".plist")}
+        unset HISTFILE url secret
 
-    Step 3: Start the Jenkins Slave via brew services
+      Step 3: Start the Jenkins Slave via brew services
 
-      If you want to start on machine boot, use `sudo`:
+        If you want to start on machine boot, use `sudo`:
 
-      sudo brew services start riboseinc/jenkins-slaves/jenkins-slave
+        sudo brew services start riboseinc/jenkins-slaves/jenkins-slave
 
-      If you want to start on login, just do this:
+        If you want to start on login, just do this:
 
-      brew services start riboseinc/jenkins-slaves/jenkins-slave
+        brew services start riboseinc/jenkins-slaves/jenkins-slave
 
 
-    *Ignore what brew tells you below!*
-    -------------vvvvvvv-------------
-     \\------------vvvvv------------/
-      \\------------vvv------------/
-       \\------------v------------/
-  EOS
+      *Ignore what brew tells you below!*
+      -------------vvvvvvv-------------
+      \\------------vvvvv------------/
+        \\------------vvv------------/
+        \\------------v------------/
+    EOS
   end
 
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-      <dict>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>ProgramArguments</key>
-        <key>UserName</key>
-        <string>#{ENV["USER"]}</string>
-        <array>
-          <string>/usr/libexec/java_home</string>
-          <string>-v</string>
-          <string>1.8</string>
-          <string>--exec</string>
-          <string>java</string>
-          <string>-jar</string>
-          <string>#{libexec}/remoting-#{version}.jar</string>
-          <string>-jnlpUrl</string>
-          <string>REPLACE_ME_JENKINS_URL</string>
-          <string>-secret</string>
-          <string>REPLACE_ME_JENKINS_SECRET</string>
-        </array>
-        <key>RunAtLoad</key>
-        <true/>
-        <key>KeepAlive</key>
-        <true/>
-        <key>StandardErrorPath</key>
-        <string>#{var}/log/jenkins-slave.log</string>
-        <key>StandardOutPath</key>
-        <string>#{var}/log/jenkins-slave.log</string>
-        <key>SessionCreate</key>
-        <true/>
-      </dict>
-    </plist>
-  EOS
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+        <dict>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>ProgramArguments</key>
+          <key>UserName</key>
+          <string>#{ENV["USER"]}</string>
+          <array>
+            <string>/usr/libexec/java_home</string>
+            <string>-v</string>
+            <string>1.8</string>
+            <string>--exec</string>
+            <string>java</string>
+            <string>-jar</string>
+            <string>#{libexec}/remoting-#{version}.jar</string>
+            <string>-jnlpUrl</string>
+            <string>REPLACE_ME_JENKINS_URL</string>
+            <string>-secret</string>
+            <string>REPLACE_ME_JENKINS_SECRET</string>
+          </array>
+          <key>RunAtLoad</key>
+          <true/>
+          <key>KeepAlive</key>
+          <true/>
+          <key>StandardErrorPath</key>
+          <string>#{var}/log/jenkins-slave.log</string>
+          <key>StandardOutPath</key>
+          <string>#{var}/log/jenkins-slave.log</string>
+          <key>SessionCreate</key>
+          <true/>
+        </dict>
+      </plist>
+    EOS
   end
 
   plist_options :startup => true
